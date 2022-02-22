370c64c8f59a719546e25fa4b5b94fa028f15652
Karen Payne <paynekaren@comcast.net>
2022-02-22 12:27:55 -0800

    added worker class

---
+++ BaseNetCoreClassProject1/Classes/Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseNetCoreClassProject1.Interfaces;
+
+namespace BaseNetCoreClassProject1.Classes
+{
+    public class Worker
+    {
+
+        public static void CompareValue<T>(List<T> sender) where T : class
+        {
+            foreach (var item in sender)
+            {
+                if (item is IBase data)
+                {
+                    Debug.WriteLine(data.Id);
+                }
+
+            }
+        }
+
+    }
+
+}
